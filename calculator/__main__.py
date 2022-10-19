3e64163cbe061e1865a06367bb6042b6aa5342a8
Danil Sidoruk <danilsidoruknv@yandex.ru>
2022-10-19 22:19:13 +0300

    Initial commit

---
+++ calculator/__main__.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+# YOUR CODE HERE
+
+
+def main():
+	app = QApplication(sys.argv)
+
+	# YOUR CODE HERE
+
+	sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+	main()
